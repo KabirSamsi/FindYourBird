ba959bfc87112f57be204fd7828c6aafc4813ab0
KabirSamsi <35539143+KabirSamsi@users.noreply.github.com>
2019-03-31 15:16:12 -0700

    Changed some stuff

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookie_parser = require('cookie-parser')
 
 const app = express()
 
+app.set('views', __dirname + '/views');
 app.set('view engine', "pug")
 app.use(body_parser.urlencoded({extended: false}))
 app.use(cookie_parser())
