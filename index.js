f0e6090920edf504fd5387d7667c7aaa6edc6be1
KabirSamsi <35539143+KabirSamsi@users.noreply.github.com>
2019-03-31 22:17:32 -0700

    Removed app directory

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cookie_parser = require('cookie-parser')
 
 const app = express()
 
-app.set('views', __dirname + '/app/views');
+app.set('views', __dirname + '/views');
 app.set('view engine', "pug")
 app.use(body_parser.urlencoded({extended: false}))
 app.use(cookie_parser())
