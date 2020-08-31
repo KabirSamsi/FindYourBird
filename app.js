f5423752a0f3db5dfd43738bb319bbcba12359f7
KabirSamsi <samsikabir@gmail.com>
2020-08-31 00:30:23 -0700

    Reconfigured database setup

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ const fs = require('fs')
 const mongoose = require('mongoose');
 const http = require('http').createServer(app);
 
-// "mongodb+srv://dbUser:dbUserPassword@cluster0.h3f4r.mongodb.net/<FindYourBird>?retryWrites=true&w=majority"
-
-// mongoose.connect("mongodb+srv://dbUser:dbUserPassword@cluster0.h3f4r.mongodb.net/<FindYourBird>?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://dbUser:dbUserPassword@cluster0.h3f4r.mongodb.net/<FindYourBird>?retryWrites=true&w=majority', { useNewUrlParser: true })
+//Connect to database
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://dbUser:dbUserPassword@cluster0.h3f4r.mongodb.net/<FindYourBird>?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 .then(connect => console.log('connected to mongodb..'))
 .catch(e => console.log('could not connect to mongodb', e))
 
